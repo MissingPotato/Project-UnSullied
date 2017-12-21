78c5a736e20471e9182109de30b14c86337a3841
MissingPotato <potatomissing@gmail.com>
2017-12-22 01:44:00 +0200

    Update 0.4.1a
    + Main Menu
    + Options
    + Working Options
    + Working MainMenu
    + PlaceHolderMenuGraphics
    + Fixed Bugs
    + Added fonts
    + Added some models
    + Changed the wall model
    
    * Fix the menus
    * Add a pause menu
    * FINISH THE DARN BUILDING SYSTEM ALREADY :angry:

---
+++ Assets/#Scripts/Menu/MenuScript.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	private void Start()
+	{
+		playMenu.SetActive(false);
+		optionsMenu.SetActive(false);
+	}
+
+	[Header("Scene Swapper")]
+	[Space]
+
+	public GameObject screenLoading;
+	public Slider slider;
+
+	public List<GameObject> screenOthers;
+
+	[Space]
+	[Space]
+	[Space]
+
+	[Header("Menu Controller")]
+
+	public GameObject playMenu;
+	public GameObject optionsMenu;
+
+	// ------------------------------------------------------------------------------------------------------------------------
+
+	public void ChangeMenu(int choice)
+	{
+
+		if (choice == 0 )
+		{
+			playMenu.SetActive(false);
+			optionsMenu.SetActive(false);
+		}
+
+		else
+			if (choice == 1)
+		{
+			playMenu.SetActive(true);
+			optionsMenu.SetActive(false);
+		}
+
+		else
+			if (choice == 2 )
+		{
+			playMenu.SetActive(false);
+			optionsMenu.SetActive(true);
+		}
+
+	}
+
+	// ------------------------------------------------------------------------------------------------------------------------
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	// ------------------------------------------------------------------------------------------------------------------------
+
+	public void LoadScene(string name)
+	{
+		StartCoroutine(LoadAsynchronously(name));
+	}
+	
+	IEnumerator LoadAsynchronously (string name)
+	{
+		AsyncOperation operation = SceneManager.LoadSceneAsync(name);
+
+		foreach (GameObject scene in screenOthers)
+		{
+			scene.SetActive(false);
+		}
+
+		screenLoading.SetActive(true);
+
+		while (!operation.isDone)
+		{
+			float progress = Mathf.Clamp01(operation.progress / .9f);
+
+			slider.value = progress;
+
+			yield return null;
+		}
+
+	}
+
+
+
+}
---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour {
+
+	Resolution[] resolutions;
+
+	public Dropdown resolutionDropdown;
+
+	private void Start()
+	{
+		InitializeResolutions();
+
+	}
+
+	public void SetResolution(int resolutionIndex)
+	{
+		Resolution rez = resolutions[resolutionIndex];
+		Screen.SetResolution(rez.width, rez.height, Screen.fullScreen);
+	}
+
+	public void SetQuallity (int quallityIndex)
+	{
+		QualitySettings.SetQualityLevel(quallityIndex);
+	}
+
+	public void SetFullscreen (bool isFullscreen)
+	{
+		Screen.fullScreen = isFullscreen;
+	}
+
+	void InitializeResolutions()
+	{
+		resolutions = Screen.resolutions;
+
+		int currentResolutionIndex = 0;
+
+		resolutionDropdown.ClearOptions();
+
+		List<string> options = new List<string>();
+
+		for (int i = 0; i < resolutions.Length; i++)
+		{
+			string option = resolutions[i].width + "x" + resolutions[i].height;
+			options.Add(option);
+
+			if ( resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height )
+				currentResolutionIndex = i;
+		}
+
+		resolutionDropdown.AddOptions(options);
+		resolutionDropdown.value = currentResolutionIndex;
+		resolutionDropdown.RefreshShownValue();
+	}
+
+}
